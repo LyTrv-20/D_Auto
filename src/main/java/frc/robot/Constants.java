1c09bf6d6dd5ac9f71e87e8ce43ff6996ce8e456
Reagan Nam <reagandohyunnam@gmail.com>
2024-05-09 18:41:59 -0400

    added drivetrain, path planner, etc - reagan

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,7 +4,6 @@
 
 package frc.robot;
 
-<<<<<<< HEAD
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
@@ -22,9 +21,6 @@ import edu.wpi.first.math.util.Units;
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
-=======
-import edu.wpi.first.math.geometry.Pose2d;
->>>>>>> b3557c805aa92291f94097f840525e5401a65174
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -34,10 +30,7 @@ import edu.wpi.first.math.geometry.Pose2d;
  * It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-
- //CONSTANTS SHOULD BE CHANGABLE FOR NEXT GAME SZNS
 public final class Constants {
-<<<<<<< HEAD
   public static class controller {
     public static final int PILOT_PORT = 0;
     public static final int COPILOT_PORT = 1;
@@ -358,31 +351,5 @@ public final class Constants {
     //       Units.degreesToRadians(0),
     //       Units.degreesToRadians(180.0));
     // }
-=======
-  //Game constraints should change according to da rules 
-  public static class GameConstraints{
-    public static final int TIME_LIMIT = 15;
-    public static final int FOUL = 2;
-    public static final int TECH_FOUL = 5;
-
-    public static final int ALLOWED_GAME_ELEMENT = 1;
-    public static final int NOTE = -5; //negative bc less =  higher priority
-    //TODO: add distance restriction for shooting -> no farther than the wing tape mark in AUTO -> no full court
-    //TODO: add priority in avoiding robots AFTER passing the center line 
-  }
-
-  public static enum STATE{
-    NEW, OPEN, CLOSED
-  }
-
-  public static class KnownStaticMapPos {
-    public static final Pose2d STAGE_PODIUM_CLOSE = new Pose2d();
-    public static final Pose2d STAGE_PODIUM_AMP = new Pose2d();
-    public static final Pose2d STAGE_PODIUM_SOURCE = new Pose2d();
-    public static final Pose2d WING_LINE = new Pose2d();
-    public static final Pose2d MIDLINE = new Pose2d();
-    public static final Pose2d SOURCE = new Pose2d();
-    public static final Pose2d AMP = new Pose2d();
->>>>>>> b3557c805aa92291f94097f840525e5401a65174
   }
 }
